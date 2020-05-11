758cf61ebfd9dfdfca23c401fc9811dd1d2adbd6
Mihail Denisov <denisov.mihail2013@yandex.ru>
2020-05-11 22:15:49 +0700

    Delete extra files

--- board.h
+++
@@ -1,7 +0,0 @@
-#ifndef board_H
-#define board_H
-#include <stdio.h>
-
-void move(char board[9][9]);
-
-#endif
--- board_print_plain.h
+++
@@ -1,7 +0,0 @@
-#ifndef board_print_plain_H
-#define board_print_plain_H
-#include <stdio.h>
-
-void print_board(char board[9][9]);
-
-#endif
